fcdad6f4e6e97aa198d1186a89d14f51b3c9fd9b
Vipparla Veeranjaneyulu <veeranjaneyuluvipparla.2109@gmail.com>
2025-06-08 23:10:01 +0530

    fix: css paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // For Vercel (use '/' instead of '/my-portfolio/')
+   build: {
+    cssCodeSplit: true, // Ensures CSS is generated
+    assetsDir: 'assets', // Organizes CSS/JS in dist folder
+  }
+  
 })
